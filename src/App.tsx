4349a523eb8b75a85027decec8383c741730c74c
Caldewood Alikula <alikulaowen@gmail.com>
2022-06-14 21:14:56 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-     Yey
+    <div className="flex justify-center m-48 text-3xl font-extrabold">
+      <header className='flex flex-col items-center'>
+        <h1>My React(Typescript + TailwindCss)</h1>
+        <p>Just getting started</p>
+      </header>
     </div>
   );
 }
